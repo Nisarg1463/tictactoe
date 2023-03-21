366e6a8893b7c528c6ea61792626b4b505dae28d
Nisarg1463 <nisarg1463@gmail.com>
2023-03-21 16:31:59 +0530

    Updates

---
+++ precache-manifest.116914a224621288b91a490ef5581d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb6135b6f1809c875c1",
+    "url": "/tictactoe_react/static/css/main.b25bdc15.chunk.css"
+  },
+  {
+    "revision": "ebb6135b6f1809c875c1",
+    "url": "/tictactoe_react/static/js/main.ebb6135b.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/tictactoe_react/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "721745703027e4ea152a",
+    "url": "/tictactoe_react/static/js/2.72174570.chunk.js"
+  },
+  {
+    "revision": "e990dccd68671991bbce",
+    "url": "/tictactoe_react/static/js/runtime~main.e990dccd.js"
+  },
+  {
+    "revision": "28de40828789e1175668f706666fb2d1",
+    "url": "/tictactoe_react/index.html"
+  }
+];
